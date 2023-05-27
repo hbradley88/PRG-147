9bd221b4631e86e49372af95f4973ebd52a9f314
Heidi Bradley <hbradley@students.mchenry.edu>
2023-05-27 14:11:05 -0500

    assessment 01

---
+++ assessment-01/scripts/main.js
@@ -0,0 +1,31 @@
+function change1(){
+    document.getElementById('dog').src='images/bella.jpg';
+}
+
+function change2(){
+    document.getElementById('dog').src='images/baylee.jpg';
+}
+
+function change3(){
+    document.getElementById('dog').src='images/baxter.jpg';
+}
+
+function changeExt(){
+    document.getElementById('info').innerHTML='Bella was the Best Dog';
+}
+
+function changeImage(){
+    document.getElementById("dog").width = "400";
+}
+
+function changeText(){
+    document.getElementById('info').style.color='Green';
+}
+
+function show(){
+    document.getElementById('dog').style.display='block';
+}
+
+function hide(){
+    document.getElementById('dog').style.display='none';
+}

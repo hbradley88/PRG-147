3f5b41faae23c95373a301e8fa536b801cb191ef
Heidi Bradley <hbradley@students.mchenry.edu>
2023-06-08 03:15:30 -0500

    mod 2 assess 2 upload

---
+++ mod2-assess-2/scripts/myScript.js
@@ -0,0 +1,88 @@
+const food1 = {
+name: "Mushroom Lovers' French Bread Pizzas",
+category: "Dinner",
+image: "images/mushroom.jpeg",
+ingredients: "8 large portobello mushroom caps, 1 pound button mushrooms, 1/2 pound shiitake mushrooms, 3 tablespoons extra-virgin olive oil, 3 turns of the pan, 2 tablespoons butter, cut into pieces, 1 bay leaf, 4 large cloves garlic, finely chopped, salt and pepper, 1/2 cup dry white wine, eyeball it. 2 teaspoons Worcestershire sauce, eyeball it, 1 tablespoon chopped fresh thyme leaves, 4 sprigs stripped and chopped,1 loaf crusty French bread, 24 inches, 3 cups shredded Gruyere or Swiss cheese",
+directions: "Wipe mushrooms clean with a damp towel. Slice the mushrooms. Heat a deep skillet with extra-virgin olive oil and butter over medium heat. When butter melts into oil, add bay, garlic and mushrooms. Cook until mushrooms are dark and tender, 12 to 15 minutes. Season the mushrooms with salt and pepper and add wine. Deglaze the pan with wine, eyeball the amount. Shake the pan and add the Worcestershire and thyme. Turn off heat." + "Preheat broiler and split the loaf lengthwise and across. Hollow out a bit of the soft insides. Toast the bread lightly under broiler. Fill bread with mushrooms evenly then top with cheese liberally. Melt cheese until it browns and bubbles then remove the pizzas from the oven and turn off the broiler.",
+source: "https://www.foodnetwork.com/recipes/rachael-ray/mushroom-lovers-french-bread-pizzas-recipe-1916993"
+};
+
+const food2 = {
+name: "Foil-Packet French Bread Pizzas",
+category: "Dinner",
+image: "images/foilpacket.jpeg",
+ingredients: "1 clove garlic, grated, 6 tablespoons extra-virgin olive oil, plus more for drizzling, 1 loaf soft French or Italian bread (14 to 18 inches long), split lengthwise, then halved crosswise, 1 cup shredded whole-milk mozzarella cheese, 1 cup whole-milk ricotta cheese, 1/2 teaspoon dried oregano, 1/4 teaspoon red pepper flakes, Kosher salt, 1/2 cup sun-dried tomatoes in oil, drained and chopped, 4 cups baby arugula, 1/2 cup marinated artichoke hearts, drained and chopped, 2 ounces thinly sliced prosciutto, torn",
+directions: "Preheat a grill to medium. Stir together the grated garlic and olive oil in a small bowl, then brush on the cut side of the bread pieces. Top with the mozzarella, then the ricotta (about 4 small dollops per piece of bread). Sprinkle with the oregano, red pepper flakes and a pinch of salt. Scatter the chopped sun-dried tomatoes on top." + "Place each piece of bread on an 18-inch-long sheet of heavy-duty foil. Bring the two short ends of the foil together and fold twice; fold in the sides to seal." + "Grill the foil packets, covered, until the bottom of the bread is toasted and the cheese is melted, 8 to 10 minutes." + "Meanwhile, toss the arugula, artichokes and a pinch of salt in a bowl. Carefully open the foil packets and top the pizzas with the prosciutto and arugula salad; drizzle with olive oil.",
+source: "https://www.foodnetwork.com/recipes/food-network-kitchen/foil-packet-french-bread-pizzas-7530331"
+};
+
+const food3 = {
+name: "Grilled French Bread Pizza with Mushroom Pesto and Fontina Cheese",
+category: "Dinner",
+image: "images/pesto.jpeg",
+ingredients: "1-ounce dried porcini mushrooms, 1 pound cremini or white button mushrooms, stems removed, 1/4 cup canola oil, Salt and freshly ground black pepper, Whole head roasted garlic, pulp removed, 1/4 cup toasted walnuts, 1/4 cup loosely packed fresh flat-leaf parsley, 2 teaspoons fresh thyme leaves, 3/4 cup extra-virgin olive oil, 1/4 cup plus 2 tablespoons grated Pecorino Romano cheese, 1 loaf French bread, about 24 inches long, sliced in 1/2 lengthwise, 2 1/2 cups grated fontina cheese",
+directions: "Place the porcini mushrooms in a bowl and cover with boiling water. Let sit until softened, 30 minutes. Heat the grill to high." + "Brush the cremini or white button mushroom caps with the canola oil and season with salt and pepper. Place the mushrooms on the grill, cap side down and grill until golden brown and slightly charred, 4 to 5 minutes. Turn the caps over and continue grilling until the mushrooms are cooked through, about 5 minutes longer. Remove from the grill, let cool slightly then coarsely chop. Remove the porcinis from the soaking liquid and place in the bowl of a food processor with a few tablespoons of the soaking liquid. Add cremini mushrooms, garlic pulp, walnuts, parsley and thyme in a food processor and process until finely chopped. With the motor running, slowly add 1/2 cup of the olive oil and process until smooth. Add 1/4 cup of the Romano cheese and pulse a few times to incorporate. Season with salt and pepper." + "Brush the bread on the cut side with the remaining 1/4 cup of olive oil and season with salt and pepper. Evenly spread the pesto over the cut side of each half of bread. Top the pesto with the fontina cheese, return to the grill, cheese side up, close the cover of the grill and cook until the cheese has melted, about 2 minutes. Remove from the grill and sprinkle with the remaining Romano.",
+source: "https://www.foodnetwork.com/recipes/bobby-flay/grilled-french-bread-pizza-with-mushroom-pesto-and-fontina-cheese-recipe-1950225"
+};
+
+const food4 = {
+name: "Super Stuffed French Bread Pizza Rustica",
+category: "Dinner",
+image: "images/stuffed.jpeg",
+ingredients: "1 (2 foot long) loaf French bread, 1 pound sweet Italian sausage, 1 tablespoon extra-virgin olive oil, 1 small red bell pepper, seeded and chopped, 1 small onion, chopped, 2 large cloves garlic, chopped, 1 package frozen chopped spinach defrosted and squeezed dry, Salt and pepper, 1 1/2 cups part skim ricotta, 1/2 cup grated Parmesan, 1/2 pound sweet sopressata, from the deli, sliced thick, chopped, 1/2 stick pepperoni, chopped, 1 sack (10 ounces) shredded mozzarella, 1 sack (10 ounces) shredded provolone, 1 teaspoon dried oregano, 1 teaspoon crushed red pepper flakes",
+directions: "Preheat oven to 425 degrees F. Split bread lengthwise and hollow it out. Cut in half across, making 4 shells for pizzas. Heat a skillet over medium high flame and brown sausage in extra-virgin olive oil. Brown and crumble sausage. Add red bell pepper, onion and garlic. Cook 3 to 5 minutes, add spinach. Remove mixture from heat and season with a pinch of salt and black pepper, to your taste. Transfer to a bowl. Combine sausage and veggies with ricotta, Parmesan, sopressata and pepperoni. Fill bread shells and top with mounded mozzarella and provolone cheeses. Place in hot oven on cookie sheet and bake until cheese melts and bubbles and bread is super crisp, about 10 to 12 minutes. Top pizzas with oregano and hot pepper flakes. Serve immediately, or snack all night!",
+source: "https://www.foodnetwork.com/recipes/rachael-ray/super-stuffed-french-bread-pizza-rustica-recipe-1942239"
+};
+
+const food5 = {
+name: "Caprese French Bread Pizzas",
+category: "Dinner",
+image: "images/caprese.jpeg",
+ingredients: "1/4 cup pine nuts, 1 cup fresh basil, plus more for garnish , 2/3 cup olive oil , 1/2 cup grated Parmesan, 1 teaspoon grated lemon zest, 2 cloves garlic, minced, 1/2 teaspoon kosher salt, Freshly ground black pepper, 1 small Italian or French bread loaf, 12 ounces fresh mozzarella, sliced, 1 1/2 cups cherry tomatoes, halved, 1 tablespoon balsamic glaze",
+directions: "Preheat the oven to 425 degrees F.For the pesto: Toast the pine nuts in a small skillet over a medium heat, tossing occasionally, until golden brown in spots, 3 to 4 minutes.Combine the pine nuts, basil, olive oil, Parmesan, lemon zest, garlic, salt and a few grinds of pepper in a blender or food processor until smooth and bright green.Cut the bread in half lengthwise. Put the bread on a baking sheet, cut-side up, and spread each piece with a few tablespoons of the pesto, saving any leftovers for another recipe.Top the bread with the mozzarella slices and bake until the cheese is melted, 12 to 15 minutes.Divide the tomatoes between the two pizzas and drizzle over the balsamic glaze. Tear some basil and sprinkle on top. Serve!",
+source: "https://www.foodnetwork.com/recipes/ree-drummond/caprese-french-bread-pizzas-9904193"
+};
+    
+
+function display() {
+let html1 = `<h2>${food1.name}</h2>` +
+`<h3>${food1.category}</h3>` + `<img src = ${food1.image}>` +
+`<h3>Ingredients</h3>` +
+`<p>${food1.ingredients}<p>` + `<h3>Directions</h3>` +
+`<p>${food1.directions}</p>` + `<h3>Source</h3>` +
+`<p>${food1.source}</p>`;
+document.getElementById('ob1').innerHTML = html1;
+
+let html2 = `<h2>${food2.name}</h2>` +
+`<h3>${food2.category}</h3>` + `<img src = ${food2.image}>` +
+`<h3>Ingredients</h3>` +
+`<p>${food2.ingredients}<p>` + `<h3>Directions</h3>` +
+`<p>${food2.directions}</p>` + `<h3>Source</h3>` +
+`<p>${food2.source}</p>`;
+document.getElementById('ob2').innerHTML = html2;
+
+let html3 = `<h2>${food3.name}</h2>` +
+`<h3>${food3.category}</h3>` + `<img src = ${food3.image}>` +
+`<h3>Ingredients</h3>` +
+`<p>${food3.ingredients}<p>` + `<h3>Directions</h3>` +
+`<p>${food3.directions}</p>` + `<h3>Source</h3>` +
+`<p>${food3.source}</p>`;
+document.getElementById('ob3').innerHTML = html3;
+
+let html4 = `<h2>${food4.name}</h2>` +
+`<h3>${food4.category}</h3>` + `<img src = ${food4.image}>` +
+`<h3>Ingredients</h3>` +
+`<p>${food4.ingredients}<p>` + `<h3>Directions</h3>` +
+`<p>${food4.directions}</p>` + `<h3>Source</h3>` +
+`<p>${food4.source}</p>`;
+document.getElementById('ob4').innerHTML = html4;
+
+let html5 = `<h2>${food5.name}</h2>` +
+`<h3>${food5.category}</h3>` + `<img src = ${food5.image}>` +
+`<h3>Ingredients</h3>` +
+`<p>${food5.ingredients}<p>` + `<h3>Directions</h3>` +
+`<p>${food5.directions}</p>` + `<h3>Source</h3>` +
+`<p>${food5.source}</p>`;
+document.getElementById('ob5').innerHTML = html5;
+}
+

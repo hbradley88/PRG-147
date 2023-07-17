53fd36d31243fddd3859a445132eca73ac5caa22
Heidi Bradley <hbradley@students.mchenry.edu>
2023-07-17 09:54:02 -0500

    M2A1

--- mod2-assess-1/scripts/calculate.js
+++ mod2-assess-1/scripts/calculate.js
@@ -1,22 +1,22 @@
-// function setValues(){
-//     let today = new Date();
-//     document.getElementById("name").value = "";
-//     document.getElementById("sand1").value = "";
-//     document.getElementById("sand2").value = "";
-//     document.getElementById("sand3").value = "";
-//     document.getElementById("sand4").value = "";
-//     document.getElementById("sand5").value = "";
-//     document.getElementById("drink1").value = "";
-//     document.getElementById("drink2").value = "";
-//     document.getElementById("drink3").value = "";
-//     document.getElementById("drink4").value = "";
-//     document.getElementById("drink5").value = "";
-//     document.getElementById("dessert1").value = "";
-//     document.getElementById("dessert2").value = "";
-//     document.getElementById("dessert3").value = "";
-//     document.getElementById("dessert4").value = "";
-//     document.getElementById("dessert5").value = "";
-// }
+function setValues(){
+    let today = new Date();
+    document.getElementById("name").value = "";
+    document.getElementById("sand1").value = "";
+    document.getElementById("sand2").value = "";
+    document.getElementById("sand3").value = "";
+    document.getElementById("sand4").value = "";
+    document.getElementById("sand5").value = "";
+    document.getElementById("drink1").value = "";
+    document.getElementById("drink2").value = "";
+    document.getElementById("drink3").value = "";
+    document.getElementById("drink4").value = "";
+    document.getElementById("drink5").value = "";
+    document.getElementById("dessert1").value = "";
+    document.getElementById("dessert2").value = "";
+    document.getElementById("dessert3").value = "";
+    document.getElementById("dessert4").value = "";
+    document.getElementById("dessert5").value = "";
+}
 
 let sandSub = 0
 let drinkSub = 0
@@ -24,26 +24,48 @@ let dessertSub = 0
 let grandTotal = 0
 
 function sanSubTotal(){
+    sandSub = 0
     let checkboxes = document.getElementsByName("sandwich");
     for (let i = 0; i < checkboxes.length; i++){
         if(checkboxes[i].checked){
-            sandSub += checkboxes[i].value
+            sandSub += 1
         }
     }
-    console.log("sandsub = " + sandSub) 
+    sandSub *= 10
+    return sandSub     
 }
 
-function getTotal(){
-    sanSubTotal()
+function drinkSubTotal(){
+    drinkSub = 0
+    let checkboxes = document.getElementsByName("drink");
+    for (let i = 0; i < checkboxes.length; i++){
+        if(checkboxes[i].checked){
+            drinkSub += 1
+        }
+    }
+    drinkSub *= 2
+    return drinkSub     
+}
+
+function dessertSubTotal(){
+    dessertSub = 0
+    let checkboxes = document.getElementsByName("dessert");
+    for (let i = 0; i < checkboxes.length; i++){
+        if(checkboxes[i].checked){
+            dessertSub += 1
+        }
+    }
+    dessertSub *= 4
+    return dessertSub     
 }
 
 function total(){
     let name = document.getElementById("name").value;
-    let sandTotal = parseFloat(document.getElementById("sandwich").value);
     
-    let sandCost = (sand1 + sand2 + sand3 + sand4 + sand5)
-    document.getElementById("meal").value = cost;
-
-    var totalCost = cost;
-    document.getElementById("total").innerHTML= (name + ", your total is: " + cost);
+    let sanSub = sanSubTotal()
+    let drinkSub = drinkSubTotal()
+    let dessertSub = dessertSubTotal()
+    let grandTotal = sanSub + drinkSub + dessertSub
+    
+    document.getElementById("total").innerHTML= (name + ", your total is: " + grandTotal);
 }

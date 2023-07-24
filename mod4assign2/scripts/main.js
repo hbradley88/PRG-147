ea0101115984a18a8a3c316ea222aa4b9ce74dcc
Heidi Bradley <hbradley@students.mchenry.edu>
2023-07-24 14:24:19 -0500

    jquery2

---
+++ mod4assign2/scripts/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $("#baxterHide").click(function(){
+      $("#baxter").hide();
+    });
+
+    $("#baxterShow").click(function(){
+      $("#baxter").show();
+    });
+
+    $("#bayleeFadeIn").click(function(){
+        $("#baylee").fadeIn("slow");
+    });
+
+    $("#bayleeFadeOut").click(function(){
+      $("#baylee").fadeOut(2000);
+  });
+
+    $("#bellaSlide").click(function(){
+        $("#bella").slideToggle("slow");
+        });
+});
+

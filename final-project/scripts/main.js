27e0383976517098a7f9825e70efb209e9ce4049
Heidi Bradley <hbradley@students.mchenry.edu>
2023-07-25 23:29:49 -0500

    final project

---
+++ final-project/scripts/main.js
@@ -0,0 +1,97 @@
+// external js: masonry.pkgd.js
+
+$('.grid').masonry({
+  itemSelector: '.grid-item',
+  columnWidth: 160
+});
+
+$(document).ready(function(){
+
+  $("#2019").click(function(){
+    $(this).attr("src", "images/2019.png");
+    $(this).attr("class", "grid-item--height3");
+  });
+
+  $("#2179").click(function(){
+    $(this).attr("src", "images/2179.png");
+    $(this).attr("class", "grid-item--height3");
+  });
+
+  $("#2223").click(function(){
+    $(this).attr("src", "images/2223.png");
+    $(this).attr("class", "grid-item");
+  });
+
+  $("#2276").click(function(){
+    $(this).attr("src", "images/2276.png");
+    $(this).attr("class", "grid-item--width3");
+  });
+
+  $("#2312").click(function(){
+    $(this).attr("src", "images/2312.png");
+    $(this).attr("class", "grid-item--height3");
+  });
+
+  $("#2463").click(function(){
+    $(this).attr("src", "images/2463.png");
+    $(this).attr("class", "grid-item--height3");
+  });
+
+  $("#2469").click(function(){
+    $(this).attr("src", "images/2469.png");
+    $(this).attr("class", "grid-item");
+  });
+
+  $("#2612").click(function(){
+    $(this).attr("src", "images/2612.png");
+    $(this).attr("class", "grid-item");
+  });
+
+  $("#2648").click(function(){
+    $(this).attr("src", "images/2648.png");
+    $(this).attr("class", "grid-item--height3");
+  });
+
+  $("#2621").click(function(){
+    $(this).attr("src", "images/2621.png");
+    $(this).attr("class", "grid-item--width2");
+  });
+
+  $("#2632").click(function(){
+    $(this).attr("src", "images/2632.png");
+    $(this).attr("class", "grid-item--width2");
+  });
+
+  $("#3024").click(function(){
+    $(this).attr("src", "images/3024.png");
+    $(this).attr("class", "grid-item--width2");
+  });
+
+  $("#3100").click(function(){
+    $(this).attr("src", "images/3100.png");
+    $(this).attr("class", "grid-item");
+  });
+
+  $("#3334").click(function(){
+    $(this).attr("src", "images/3334.png");
+    $(this).attr("class", "grid-item--height3");
+  });
+
+  $("#3371").click(function(){
+    $(this).attr("src", "images/3371.png");
+    $(this).attr("class", "grid-item--height3");
+  });
+
+  $("#3390").click(function(){
+    $(this).attr("src", "images/3390.png");
+    $(this).attr("class", "grid-item--height3");
+  });
+
+  $("#2643").click(function(){
+    $(this).attr("src", "images/2643.png");
+    $(this).attr("class", "grid-item--height3");
+  });
+  
+  });
+ 
+
